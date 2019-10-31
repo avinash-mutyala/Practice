905a22ae5ace2972f15bf5524f04616632f3f119
Mutyala, Avinash <avinash.mutyala@accenture.com>
2019-10-31 10:42:04 +0530

    react sample added

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Test
     {
+        static async Task Main(string[] args)
+        {
+            var urls = new[] { "http://google.com", "http://www.pskills.org", "https://aws.amazon.com" };
+            var httpClient = new HttpClient();
+            foreach (var url in urls)
+            {
+                var response = await httpClient.GetAsync(url);
+                Console.WriteLine($"{url} - {response.IsSuccessStatusCode}");
+            }
+        }
 
         static void Main()
         {
-            Prime.PrimeNumbers();
+            //Prime.PrimeNumbers();
             //Ascii.MagicWord();
 
             Console.WriteLine();
         }
 
 
-     
+
 
 
 
---
+++ ReactSample/ClientApp/src/components/Add.js
@@ -0,0 +1,66 @@
+﻿import React, { Component } from 'react';
+
+export class Add extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { FeildA: 0, FeildB: 0, Result: 0, Operator: 0 };
+        this.handleChange = this.handleChange.bind(this);
+        this.calculate = this.calculate.bind(this);
+    }
+
+    handleChange(e) {
+        console.log(e.target.value === "");
+        if (e.target.value === "" || e.target.value == null) {
+            this.setState({
+                [e.target.name]: ""
+            });
+        }else if (!isNaN(Number(e.target.value))) {
+            this.setState({ [e.target.name]: Number(e.target.value) });
+        }
+        console.log(e.target.name + " " + e.target.value);
+    }
+
+    calculate() {
+        console.log(this.state.FeildA + " " + this.state.FeildB);
+        let value = this.state.Result;
+        if (this.state.Operator === 0) {
+            value = this.state.FeildA + this.state.FeildB;
+        } else if (this.state.Operator === 1) {
+            value = Math.abs(this.state.FeildA - this.state.FeildB);
+        } else if (this.state.Operator === 2) {
+            value = this.state.FeildA * this.state.FeildB;
+        } else if (this.state.Operator === 3) {
+            value = this.state.FeildA / this.state.FeildB;
+        } else {
+        }
+        this.setState({ Result: value });
+    }
+
+    render() {
+        const operators = [{ id: 0, value: "+" }, { id: 1, value: "-" }, { id: 2, value: "*" }, { id: 3, value: "/" }];
+
+        return (
+            <div>
+                <h1>Addition</h1>
+
+                <input type="text" name="FeildA" value={this.state.FeildA} onChange={(e) => this.handleChange(e)} />
+
+                <span>  </span>
+                <select name="Operator" value={this.state.Operator} onChange={(e) => this.handleChange(e)}>
+                    {operators.map(op => (
+                        <option key={op.id} value={op.id}>{op.value}</option>
+                    ))}
+                </select>
+                <span>  </span>
+                <input type="text" name="FeildB" value={this.state.FeildB} onChange={(e) => this.handleChange(e)} />
+                <span> = {this.state.Result} </span>
+                <br /><br />
+                <p>
+                    <button className="btn btn-primary" onClick={this.calculate}>Calculate</button>
+                </p>
+            </div>
+
+        );
+    }
+}
